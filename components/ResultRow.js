ac8b7181aa4e9e937a0819d3bba64c44af733b0f
Saberstacks <saber7email@gmail.com>
2024-11-20 22:41:12 -0500

    Update ResultRow.js

--- components/ResultRow.js
+++ components/ResultRow.js
@@ -11,14 +11,9 @@ export default function ResultRow({ data, type }) {
     }
   };
 
-  if (!data || (!data.business_name && !data.page_title)) return null;
-
   return (
     <div className="result-row">
-      <h4>
-        {type === 'map' ? data.business_name : data.page_title} (Rank:{' '}
-        {type === 'map' ? data.rank_in_map_pack : data.rank_in_organic})
-      </h4>
+      <h4>{type === 'map' ? data.business_name : data.page_title}</h4>
       <p>{type === 'map' ? data.address : data.page_description}</p>
       <button onClick={handleAnalyze}>Analyze</button>
       <style jsx>{`
@@ -28,10 +23,10 @@ export default function ResultRow({ data, type }) {
           margin-bottom: 10px;
         }
         h4 {
-          margin: 0 0 5px 0;
+          margin: 0 0 5px;
         }
         p {
-          margin: 0 0 10px 0;
+          margin: 0 0 10px;
         }
       `}</style>
     </div>
